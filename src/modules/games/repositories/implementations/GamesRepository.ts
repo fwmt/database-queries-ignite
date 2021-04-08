4a1b6435797cc2fbc2137fd77da24e23dcc5361d
Filipe Tavares <fwmtavares@gmail.com>
2021-04-08 03:39:48 -0200

    Desafio concluido

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -13,18 +13,26 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
-    return this.repository
-      .createQueryBuilder()
+     const games = await this.repository
+      .createQueryBuilder('games')
+      .where(`LOWER(title) LIKE '%${param.toLowerCase()}%'`)
+      .getMany();
       // Complete usando query builder
+      return games;
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository
+    .query(`SELECT COUNT(*) FROM games`); // Complete usando raw query
   }
 
-  async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+  async findUsersByGameId(id: string): Promise<User[]> {    
+    const users = await getRepository(User).createQueryBuilder('users')
+    .innerJoin('users.games','games')
+    .where(`games.id = :id`, { id })
+    .getMany()
+    
+    return users;
+      //Complete usando query builder
   }
 }
